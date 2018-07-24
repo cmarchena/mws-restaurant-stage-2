86305e65f4f90deb16f502f7c03da3b79eb20f5c
Carlos Marchena <LeanLawyering@MacBook-Pro-de-Carlos.local>
2018-07-24 18:12:57 +0200

    Include manifest and Lighthouse reports

--- front/src/assets/js/test-db.js
+++ front/src/assets/js/test-db.js
@@ -2,26 +2,25 @@ var dbPromise = idb.open('restaurants-json', 1, function (upgradeDb) {
   var store = upgradeDb.createObjectStore('json', {
     keyPath: 'id'
   });
-
   store.createIndex('by-name', 'name');
   store.createIndex('by-location', 'neighborhood');
 });
 
+fetch("http://localhost:1337/restaurants")
+  .then(res => res.json())
+  .then(json => storeItems(json))
 
+function storeItems(data) {
 
+  dbPromise.then(function (db) {
+    if (!db) return;
+    var tx = db.transaction('json', 'readwrite');
+    var store = tx.objectStore('json');
+    data.forEach(function (message) {
+      console.log(message);
+      store.put(message)
 
-
-
-dbPromise.then(function (db) {
-  if (!db) return;
-  var tx = db.transaction('json', 'readwrite');
-  var store = tx.objectStore('json');
-  fetch("http://localhost:1337/restaurants")
-    .then(res => res.json())
-    .then(json => json.forEach(function (message) {
-        console.log(message)
-
-      })
-
-    )
-});
+      return tx.complete;
+    })
+  })
+}
